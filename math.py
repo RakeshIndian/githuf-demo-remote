9053cef342561e682a09b5f5d84722ccf531d504
RakeshIndian <68988265+RakeshIndian@users.noreply.github.com>
2022-01-19 21:00:36 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#Add implementation
+def addition(x,y):
+    return (x+y)
+#Add implementation #on master
+def subtraction(x,y):
+        return (x-y)
+#Add implementation
+def multiplication(x,y):
+    return x*y
+#Add implementation
+def division(x,y):
+    if(y==0): return DIVIDE_BY_ZERO_ERROR
+    else: return x/y
+def square(x):
+    pass
